c0ffcccb710cbde5219a7f7657131a8983605824
NatanBudman <natanbd02@gmail.com>
2022-10-25 16:33:32 -0300

    upgrateUI

--- Assets/Scripts/Menu/Levels.cs
+++ Assets/Scripts/Menu/Levels.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,49 +6,167 @@ using UnityEngine.UI;
 
 public class Levels : MonoBehaviour
 {
+    [SerializeField] private GameRules _rules;
+    [Space]
+    
     #region Maps
+        [Header("MAP")]
+    public GameObject PanelsMaps;
 
-      public GameObject PanelsMaps;
+    public Image Map;
 
-      [SerializeField] private Transform[] PointsToMoveMaps;
+    [SerializeField] private string[] SceneName; 
     
-        public Dropdown DropMapsName;
+    public Dropdown DropMapsName;
     
-        private int VerifiqueChangeOptions;
-
-        private float speed = 320;
-
-        [SerializeField] private Transform AusPosition;
+    private int VerifiqueChangeOptions;
     #endregion
-  
+
+    #region GameRulesSelection
+
+    private int IADificult
+    {
+        get => _rules.DificultIA;
+        set => _rules.DificultIA = value;
+    }
+
+    [SerializeField] private Text iaDifText;
+
+    private float TimeBoxSpawn
+    {
+        get => _rules.TimeToSpawnBox;
+        set => _rules.TimeToSpawnBox = value;
+    }
+
+    [SerializeField] private Text timeBoxText;
+
+
+    [SerializeField] private Slider TimeBoxSlider;
+    [SerializeField] private Slider iaSlider;
+
+    private bool isHasBalance => _rules.IsHasBalance;
+
+    private bool isUpdateUI;
+
+    #endregion
     
     // Start is called before the first frame update
     void Start()
     {
+        Map.sprite = DropMapsName.options[0].image;
         
+        iaSlider.onValueChanged.AddListener(ChangeDificultSlider);
+        TimeBoxSlider.onValueChanged.AddListener(ChangeTimeBoxSlider);
+
+        UpdateUI();
+
     }
 
-    // Update is called once per frame
+  
+
+
     void Update()
     {
         if (VerifiqueChangeOptions != DropMapsName.value)
         {
-            var velocity = speed * Time.deltaTime;
-            
-           
-            AusPosition.position = PointsToMoveMaps[DropMapsName.value].position;
-            
-            if( PanelsMaps.transform.position.y != AusPosition.position.y)
-            {
-                PanelsMaps.transform.position = Vector3.MoveTowards( PanelsMaps.transform.position,-AusPosition.position,velocity);
-            }
-
-
-            if ( PanelsMaps.transform.position.y == AusPosition.position.y)
-            {
-                VerifiqueChangeOptions = DropMapsName.value;
-            }
+            Map.sprite = DropMapsName.options[DropMapsName.value].image;
+            VerifiqueChangeOptions = DropMapsName.value;
         }
-       
+
+        if (IADificult != _rules.DificultIA)
+        {
+            switch (IADificult)
+            {
+                case 1:
+                    iaDifText.text = "Easy AI";
+                    iaDifText.color = new Color(0, 255, 0);
+                    break;
+                case 2:
+                    iaDifText.text = "Normal AI";
+                    iaDifText.color = new Color(0, 0, 255);
+
+                        break;
+                    case 3:
+                        iaDifText.text = "Expert AI";
+                        iaDifText.color = new Color(255, 0, 0);
+                        break;
+                    default:
+                        iaDifText.text = "Expert AI";
+                        iaDifText.color = new Color(255, 0, 0);
+                        break;
+                    
+                        
+            }
+            _rules.DificultIA = IADificult;
+            Debug.Log(IADificult);
+
+        }
+
+        if (isHasBalance != _rules.IsHasBalance)
+        {
+            _rules.IsHasBalance = isHasBalance;
+        }
+
+        if (TimeBoxSpawn != _rules.TimeToSpawnBox)
+        {
+            timeBoxText.text = "" + TimeBoxSpawn;
+            _rules.TimeToSpawnBox = TimeBoxSpawn;
+        }
+
+    }
+
+    void UpdateUI()
+    {
+            switch (IADificult)
+            {
+                case 0:
+                    iaDifText.text = "Easy AI";
+                    iaDifText.color = new Color(0, 255, 0);
+                    break;
+                case 1:
+                    iaDifText.text = "Normal AI";
+                    iaDifText.color = new Color(0, 0, 255);
+
+                    break;
+                case 2:
+                    iaDifText.text = "Expert AI";
+                    iaDifText.color = new Color(255, 0, 0);
+                    break;
+                default:
+                    iaDifText.text = "Expert AI";
+                    iaDifText.color = new Color(255, 0, 0);
+                    break;
+                    
+                        
+            }
+            _rules.DificultIA = IADificult;
+            _rules.IsHasBalance = isHasBalance;
+            timeBoxText.text = "" + TimeBoxSpawn;
+            _rules.TimeToSpawnBox = TimeBoxSpawn;
+    }
+
+    public void PlayButton()
+    {
+        string MapSelected = $"Scene/{SceneName[DropMapsName.value]}";
+        
+        if (MapSelected != String.Empty)
+        {
+            
+        }
+        else
+        {
+            Debug.Log("Scene dont exist");
+        }
+    }
+
+    public void ChangeDificultSlider(float value)
+    {
+        IADificult = (int)value;
+        UpdateUI();
+    }
+    public void ChangeTimeBoxSlider(float value)
+    {
+        TimeBoxSpawn = value;
+        UpdateUI();
     }
 }
---
+++ Assets/Scripts/SciptableObjects/GameParamets/GameRules.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Rules", menuName = "Rules")]
+public class GameRules : ScriptableObject
+{
+    [SerializeField] private Rules _rules;
+    
+    public int DificultIA
+    {
+        get => _rules.DificultIA;
+        set => _rules.DificultIA = value;
+    }
+
+    public float TimeToSpawnBox
+    {
+        get => _rules.TimeToSpawnBox;
+        set => _rules.TimeToSpawnBox = value;
+    }
+
+    public bool IsHasBalance
+    {
+        get => _rules.IsHasBalance;
+        set => _rules.IsHasBalance = value;
+    }
+}
+[System.Serializable]
+public struct Rules
+{
+    public int DificultIA;
+
+    public float TimeToSpawnBox;
+
+    public bool IsHasBalance;
+}
