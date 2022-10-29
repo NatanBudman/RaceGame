9565ca3c21ce4165c2364f64621f527fa4a17b00
JuanMBrdch <juanmbordachar@gmail.com>
2022-10-29 19:50:37 -0300

    un kart que funca

--- Assets/Scripts/Controllers/KartControllerTest.cs
+++ Assets/Scripts/Controllers/KartControllerTest.cs
@@ -8,8 +8,11 @@ public class KartControllerTest : MonoBehaviour
 
     [SerializeField] private Rigidbody sphereRb;
     [SerializeField] private float forwardSpeed;
+    [SerializeField] private float turnSpeed;
+    
     private float _forwardAmount;
     private float _currentSpeed;
+    private float _turnAmount;
 
     private void Start()
     {
@@ -22,10 +25,16 @@ public class KartControllerTest : MonoBehaviour
         transform.position = sphereRb.transform.position;
 
         _forwardAmount = Input.GetAxis("Vertical");
-
+        _turnAmount = Input.GetAxis("Horizontal");
+        
         if (_forwardAmount != 0)
             Drive();
+        else
+            NoDrive();
+
+        TurnHandler();
     }
+
 
 
     private void FixedUpdate()
@@ -33,10 +42,19 @@ public class KartControllerTest : MonoBehaviour
         sphereRb.AddForce(transform.forward * _currentSpeed, ForceMode.Acceleration);
     }
 
-    
-    
+    private void TurnHandler()
+    {
+        float newRotatation = _turnAmount * turnSpeed * Time.deltaTime;
+        transform.Rotate(0, newRotatation, 0, Space.World);
+    }
     private void Drive()
     {
         _currentSpeed = _forwardAmount *= forwardSpeed;
     }
+    
+    private void NoDrive()
+    {
+        _currentSpeed = 0;
+    }
+    
 }
